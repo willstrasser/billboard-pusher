2077073ada2ddb47cf017c852348b076e198515f
Will Strasser <wstrasser@hioscar.com>
2020-04-05 18:32:31 -0400

    first attempt at twilio validation

--- api/push-event.js
+++ api/push-event.js
@@ -1,4 +1,5 @@
 import Channels from 'pusher';
+import twilio from 'twilio';
 
 const {
   PUSHER_APP_ID: appId,
@@ -16,6 +17,24 @@ const channels = new Channels({
 
 export default (req, res) => {
   const data = req.query;
+
+  const twilioSignature = req.headers['x-twilio-signature'];
+  const params = req.query;
+  const url = 'https://billboard-pusher.now.sh/api/push-event';
+
+  const requestIsValid = twilio.validateRequest(
+    process.env.TWILIO_ACCOUNT_AUTH,
+    twilioSignature,
+    url,
+    params
+  );
+
+  console.log('requestIsValid', requestIsValid);
+
+  if (!requestIsValid) {
+    return res.status(401).send('Unauthorized');
+  }
+
   channels.trigger('my-channel', 'my-event', data, () => {
     res.status(200).end(`sent event successfully {data: ${JSON.stringify(data)}`);
   });
