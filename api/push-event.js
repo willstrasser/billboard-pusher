cd7f3ee7d926c247870a9b53d61da1f5952709d6
Will Strasser <wstrasser@hioscar.com>
2020-04-11 17:54:48 -0400

    Revert "adds logs back in for prod debugging"
    This reverts commit be13e5eb2b403bbdda09802792a4962e6311b76e.

--- api/push-event.js
+++ api/push-event.js
@@ -20,19 +20,12 @@ export default (req, res) => {
   const twilioSignature = req.headers['x-twilio-signature'];
   const url = process.env.WEBHOOK_URL + 'api/push-event';
 
-  console.log(process.env.TWILIO_ACCOUNT_AUTH);
-  console.log(twilioSignature);
-  console.log(url);
-  console.log(params);
-
   const requestIsValid = twilio.validateRequest(
     process.env.TWILIO_ACCOUNT_AUTH,
     twilioSignature,
     url,
     params
   );
-
-  console.log('requestIsValid', requestIsValid);
 
   if (!requestIsValid) {
     return res.status(401).send('Unauthorized');
